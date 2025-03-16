4096d0f390c411be7e786bebfbea1ff15961d6bf
pipinpadlaxicopeles <imamsutria14@gmail.com>
2025-03-16 16:13:46 +0700

    Updated 16m Mar 2025

---
+++ Tasbeeh Watch App/View/PrayeerTimes.swift
@@ -0,0 +1,18 @@
+//
+//  PrayeerTimes.swift
+//  Tasbeeh Watch App
+//
+//  Created by Imam Sutria on 15/03/25.
+//
+
+import SwiftUI
+
+struct PrayeerTimes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrayeerTimes()
+}
---
+++ Tasbeeh Watch App/View/TasbeehView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Tasbeeh Watch App
+//
+//  Created by Imam Sutria on 15/03/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
